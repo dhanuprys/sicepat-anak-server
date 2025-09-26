3026bc561eed17749d2b355cfe6c354419cdb4de
dhanuprys <dhanuprys@gmail.com>
2025-09-26 20:20:14 +0800

    fix: remove children relationship

--- app/models.py
+++ app/models.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, Integer, String, DateTime, Date, ForeignKey, Text, Boolean
+from sqlalchemy import Column, Integer, String, DateTime, Date, Text, Boolean
 from sqlalchemy.orm import relationship
 from sqlalchemy.sql import func
 from app.database import Base
@@ -17,6 +17,3 @@ class User(Base):
     password = Column(String(255), nullable=False)
     is_admin = Column(Boolean, nullable=True, default=False)
     registration_date = Column(DateTime, default=func.now())
-    
-    # Relationships
-    children = relationship("Children", back_populates="user", cascade="all, delete-orphan")
